da63b8ae56518bfc3e641806d1ade8435dedfae3
Daantaa <d2nutn@gmail.com>
2025-02-13 23:56:37 +0200

    Implemented Rapid API Cars and Card Details Modal

---
+++ utils/index.ts
@@ -0,0 +1,25 @@
+export async function fetchCars() {
+    const headers = {
+		'x-rapidapi-key': '831e667624msh6e8b3c3e0743dd2p145e05jsn4bbdf4e5db1e',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers,});
+    
+    const result = await response.json();
+
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05;
+  
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
